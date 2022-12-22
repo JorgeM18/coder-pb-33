8a3e9a554ea8bb735d36a93f318e871867e58e87
Jorge Malo <jorge.maloing@gmail.com>
2022-12-21 19:50:11 -0500

    Changes home endpoint

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('<h1>Recetas</h1>');
+app.get('/tarea1', (req, res) => {
+  res.send('<h1>Tarea 1</h1>');
 })
 
 app.get('/tarea2', (req, res) => {
