b4d4fc0a5b9004506fe2747d0f5070601958a4fb
Jorge Malo <jorge.maloing@gmail.com>
2022-12-21 19:24:16 -0500

    Adding home endpoint

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+// Routes
+app.get('/', (req, res) => {
+  res.send('<h1>Recetas</h1>');
+})
 
 app.listen(PORT, () => {
   console.log("Ready on port => ", PORT);
